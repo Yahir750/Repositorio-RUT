68670dd5f80d5564ff2c0e3ee676701427b7e800
yahir750 <yahirc750@gmail.com>
2024-08-21 21:25:20 -0600

    Archivos actualizados a sprint boot

--- ReUseTech/src/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Prueba de configuracion de git :) ");
-    }
-}
---
+++ ReUseTech/src/test/java/reusetech/reusetech/ReusetechApplicationTests.java
@@ -0,0 +1,13 @@
+package reusetech.reusetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReusetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
