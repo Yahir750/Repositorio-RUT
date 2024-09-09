2aefcdb1cca477a796b60ee5a6206a795a75ecb7
yahir750 <yahirc750@gmail.com>
2024-09-08 22:49:55 -0600

    Cambio de estructura MVC + parte grafica

---
+++ ReUseTech/src/main/java/Controller/Rest/rest.java
@@ -0,0 +1,4 @@
+package Controller.Rest;
+
+public class rest {
+}
---
+++ ReUseTech/src/main/java/Model/ModelSQL.java
@@ -0,0 +1,6 @@
+package Model;
+
+public class ModelSQL {
+
+    // para que se vea bien las capetas estamos por empezar con las bases de datos
+}
