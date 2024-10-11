9e821ed26838a57cdee4c3cca0aa5a8307e4d68c
Omar6743 <carlos.vrosas@alumnos.udg.mx>
2024-10-11 13:40:50 -0600

    Update and rename PaqueteService to PaqueteService.java

--- ReUseTech/src/main/java/com/ReUseTech/service/PaqueteService.java
+++ ReUseTech/src/main/java/com/ReUseTech/service/PaqueteService.java
@@ -1,7 +1,7 @@
-package service;
+package com.ReUseTech.service;
 
-import Model.Paquete;
-import Model.PaqueteRepository;
+import com.ReUseTech.model.Paquete;
+import com.ReUseTech.model.PaqueteRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
